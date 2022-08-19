a8fd8f693dd180596858cd432ef809b4d84caed3
Matthew Egan <mnegan93@gmail.com>
2022-08-20 08:23:09 +1200

    Handle party name input

--- guestbook-c-sharp/guestbook-c-sharp/Program.cs
+++ guestbook-c-sharp/guestbook-c-sharp/Program.cs
@@ -1,2 +1,7 @@
 ﻿Console.WriteLine("Welcome to the Guestbook App");
 Console.WriteLine("****************************");
+
+Console.Write("What is your party/group name: ");
+string output = Console.ReadLine();
+
+Console.WriteLine(output);
